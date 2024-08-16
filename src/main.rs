1205de2e80ffc74a901836a886e23c36d2ad55e3
Allan Somensi <allansomensidev@gmail.com>
2024-08-16 20:17:18 -0300

    feat: add `tokio_postgres`, `refinery` and `dotenv` dependencies
    - Added `tokio_postgres` for PostgreSQL database interaction.
    - Added `refinery` for migrations.
    - Added `dotenv` for environment variable management.

--- src/main.rs
+++ src/main.rs
@@ -10,9 +10,11 @@ use tokio::net::TcpListener;
 
 #[tokio::main]
 async fn main() {
+    dotenv::from_filename(".env.development").ok();
     let app = Router::new().route("/", get("Hello, world!"));
 
     let listener = TcpListener::bind("127.0.0.1:8080").await.unwrap();
+    println!("=> Listening on http://localhost:8080...");
     axum::serve(listener, app.into_make_service())
         .await
         .unwrap();
