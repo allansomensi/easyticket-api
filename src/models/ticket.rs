c1837b4b11127058c9926ab7b2d85cefd4fac281
Allan Somensi <allansomensidev@gmail.com>
2024-08-30 16:52:28 -0300

    feat: create basic `Ticket` and `NewTicket` models

--- src/models/ticket.rs
+++ src/models/ticket.rs
@@ -1,23 +1,70 @@
 use chrono::{DateTime, Utc};
 use serde::{Deserialize, Serialize};
+use uuid::Uuid;
 
-use super::user::User;
-
-#[derive(Deserialize, Serialize)]
-enum TicketStatus {
-    Open,
-    Pending,
-    Concluded,
+pub enum TicketError {
+    EmptyTitle,
+    TitleTooShort,
+    TitleTooBig,
 }
 
-#[derive(Deserialize, Serialize)]
-struct Ticket {
-    pub id: u32,
-    title: String,
-    description: Option<String>,
-    requester: User,
-    status: TicketStatus,
-    created_at: DateTime<Utc>,
-    closed_at: DateTime<Utc>,
-    closed_by: User,
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(rename_all = "snake_case")]
+pub enum TicketStatus {
+    Open,
+    Pending,
+    Completed,
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct Ticket {
+    pub id: Uuid,
+    pub title: String,
+    pub requester: String,
+    pub created_at: DateTime<Utc>,
+    pub status: TicketStatus,
+}
+
+impl Ticket {
+    pub fn new(title: &str, requester: &str) -> Result<Self, TicketError> {
+        if title.is_empty() {
+            return Err(TicketError::EmptyTitle);
+        }
+
+        if title.len() < 8 {
+            return Err(TicketError::TitleTooShort);
+        }
+
+        if title.len() > 40 {
+            return Err(TicketError::TitleTooBig);
+        }
+
+        Ok(Ticket {
+            id: Uuid::now_v7(),
+            title: String::from(title),
+            requester: String::from(requester),
+            created_at: Utc::now(),
+            status: TicketStatus::Open,
+        })
+    }
+}
+
+#[derive(Serialize, Deserialize, Clone)]
+pub struct NewTicket {
+    pub title: String,
+    pub requester: String,
+}
+
+impl NewTicket {
+    pub fn new(title: &str, requester: &str) -> Self {
+        NewTicket {
+            title: String::from(title),
+            requester: String::from(requester),
+        }
+    }
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct DeleteTicket {
+    pub id: Uuid,
 }
